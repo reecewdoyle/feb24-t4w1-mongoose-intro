b45f02a71ae3f19b35a7be01261a91b936f3ec2e
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-11-02 11:37:57 +1100

    feat: connect to MongoDB

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 // Get the port
 // Tell the server to listen to web traffic 
 
+require("dotenv").config();
+
 // Server is configured in this file, not in index.js: 
 const {app} = require("./server.js");
 const { dbConnect } = require("./utils/database.js");
