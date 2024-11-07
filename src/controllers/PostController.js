b71013bcc313caade0470745052fddc52b36e64b
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-11-07 20:55:25 +1100

    feat: post controller route

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,17 +1,27 @@
 const express = require("express");
 
 const { PostModel } = require("../models/PostModel");
-const { findOnePost } = require("../utils/crud/PostCrud");
+const { findOnePost, findManyPosts } = require("../utils/crud/PostCrud");
 
 const router = express.Router();
 
 // create route
 
 // find one route
-router.get("/:postId", async (request, response) => {
+router.get("/search/:postId", async (request, response) => {
     console.log("Searching for post with ID of: " + request.params.postId);
     let result = await findOnePost({_id: request.params.postId});
     console.log("Found post with data of: " + JSON.stringify(result));
+    response.json({
+        data: result
+    });
+});
+
+// get all posts
+router.get("/all", async (request, response) => {
+
+    let result = await findManyPosts({});
+
     response.json({
         data: result
     });
